140fcdf891754a775e65f71e24bf657a78537beb
panshiqu <panshiqu@sina.com>
2018-10-22 17:08:01 +0800

    api:ServeFiles

--- handler/api.go
+++ handler/api.go
@@ -1,7 +1,10 @@
 package handler
 
 import (
+	"io"
+	"log"
 	"net/http"
+	"os"
 )
 
 // ServeHTTP .
@@ -11,5 +14,29 @@ func ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 // ServeFiles .
 func ServeFiles(w http.ResponseWriter, r *http.Request) {
+	name := "." + r.URL.Path
 
+	fi, err := os.Stat(name)
+	if err != nil {
+		log.Println("ServeFile", err)
+		http.NotFound(w, r)
+		return
+	}
+
+	if !fi.Mode().IsRegular() {
+		log.Println("ServeFile not regular", name)
+		http.NotFound(w, r)
+		return
+	}
+
+	f, err := os.Open(name)
+	if err != nil {
+		log.Println("ServeFile", err)
+		http.NotFound(w, r)
+		return
+	}
+
+	defer f.Close()
+
+	io.Copy(w, f)
 }
