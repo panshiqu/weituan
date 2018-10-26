90b269afa8252772a1efed0805547c6315b761bd
panshiqu <panshiqu@sina.com>
2018-10-26 09:58:14 +0800

    HTTPGetJSON

--- utils/utils.go
+++ utils/utils.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"io"
 	"io/ioutil"
+	"net/http"
 )
 
 // ReadUnmarshalJSON 简单封装
@@ -14,4 +15,15 @@ func ReadUnmarshalJSON(r io.Reader, js interface{}) error {
 	}
 
 	return json.Unmarshal(data, js)
+}
+
+// HTTPGetJSON 简单封装
+func HTTPGetJSON(url string, js interface{}) error {
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	return ReadUnmarshalJSON(resp.Body, js)
 }
