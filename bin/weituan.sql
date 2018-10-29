01bf80edf9bc5924214e4e67387d74308293d4fe
panshiqu <panshiqu@sina.com>
2018-10-29 21:29:18 +0800

    foreign key and index

--- bin/weituan.sql
+++ bin/weituan.sql
@@ -3,15 +3,15 @@
 
  Source Server         : localhost
  Source Server Type    : MySQL
- Source Server Version : 50720
- Source Host           : 172.16.10.177
+ Source Server Version : 50718
+ Source Host           : localhost
  Source Database       : weituan
 
  Target Server Type    : MySQL
- Target Server Version : 50720
+ Target Server Version : 50718
  File Encoding         : utf-8
 
- Date: 10/29/2018 15:42:43 PM
+ Date: 10/29/2018 21:21:51 PM
 */
 
 SET NAMES utf8mb4;
@@ -26,7 +26,10 @@ CREATE TABLE `bargain` (
   `UserID` int(10) unsigned NOT NULL COMMENT '用户编号',
   `BargainPrice` decimal(12,2) NOT NULL COMMENT '砍价',
   `BargainTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '砍价时间',
-  PRIMARY KEY (`ShareID`,`UserID`)
+  PRIMARY KEY (`ShareID`,`UserID`),
+  KEY `UserID` (`UserID`),
+  CONSTRAINT `BargainShareID` FOREIGN KEY (`ShareID`) REFERENCES `share` (`ShareID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `BargainUserID` FOREIGN KEY (`UserID`) REFERENCES `user` (`UserID`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- ----------------------------
@@ -38,7 +41,11 @@ CREATE TABLE `share` (
   `UserID` int(10) unsigned NOT NULL COMMENT '用户编号',
   `SkuID` int(10) unsigned NOT NULL COMMENT '商品编号',
   `ShareTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '分享时间',
-  PRIMARY KEY (`ShareID`)
+  PRIMARY KEY (`ShareID`),
+  KEY `UserID` (`UserID`),
+  KEY `SkuID` (`SkuID`),
+  CONSTRAINT `ShareSkuID` FOREIGN KEY (`SkuID`) REFERENCES `sku` (`SkuID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `ShareUserID` FOREIGN KEY (`UserID`) REFERENCES `user` (`UserID`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- ----------------------------
@@ -57,7 +64,9 @@ CREATE TABLE `sku` (
   `WeChatID` varchar(255) NOT NULL COMMENT '微信号（卖家）',
   `Deadline` datetime NOT NULL DEFAULT '1970-01-01 08:00:00' COMMENT '截止时间',
   `PublishTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '发布时间',
-  PRIMARY KEY (`SkuID`)
+  PRIMARY KEY (`SkuID`),
+  KEY `UserID` (`UserID`),
+  CONSTRAINT `SkuUserID` FOREIGN KEY (`UserID`) REFERENCES `user` (`UserID`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- ----------------------------
